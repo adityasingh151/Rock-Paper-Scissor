3354b9ae01eae9672d8ae47fbe1151c3d80c43a5
Aditya Singh <122855095+adityasingh151@users.noreply.github.com>
2023-09-09 13:25:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const randomNumber1 = Math.floor(Math.random() * 3 + 1);
-const randomNumber2 = Math.floor(Math.random() * 3 + 1);
+let randomNumber1 = Math.floor(Math.random() * 3 + 1);
+let randomNumber2 = Math.floor(Math.random() * 3 + 1);
 
 document.querySelector(".img1").setAttribute("src", "./images/img" + randomNumber1 + ".jpg");
 document.querySelector(".img2").setAttribute("src", "./images/img" + randomNumber2 + ".jpg");
 
-if (randomNumber1 === randomNumber2) {
+function setPlayer2Choice(choice) {
+  randomNumber2 = choice;
+  updatePlayer1Choice(); 
+  updatePlayer2Image();
+  updateResult();
+}
+
+function updatePlayer1Choice() {
+  randomNumber1 = Math.floor(Math.random() * 3 + 1);
+  document.querySelector(".img1").setAttribute("src", "./images/img" + randomNumber1 + ".jpg");
+}
+
+function updatePlayer2Image() {
+  const img2 = document.querySelector(".img2");
+  img2.setAttribute("src", "./images/img" + randomNumber2 + ".jpg");
+}
+
+function updateResult() {
+  if (randomNumber1 === randomNumber2) {
     document.querySelector(".container h1").innerHTML = "It's a Draw";
-} else if (
+  } else if (
     (randomNumber1 === 1 && randomNumber2 === 3) ||
     (randomNumber1 === 2 && randomNumber2 === 1) ||
     (randomNumber1 === 3 && randomNumber2 === 2)
-) {
-    document.querySelector(".container h1").innerHTML = "Player 1 Wins";
-} else {
-    document.querySelector(".container h1").innerHTML = "Player 2 Wins";
+  ) {
+    document.querySelector(".container h1").innerHTML = "You lost";
+  } else {
+    document.querySelector(".container h1").innerHTML = "You won";
+  }
 }
